e2559ea609e5de977c99303de2cd7313ff6db14c
shahidul shohid <shahidulislamshohid7gmail.com>
2024-12-12 16:31:51 +0600

    use get method for getting data by method

--- index.js
+++ index.js
@@ -50,6 +50,14 @@ async function run() {
     })
 
     // job application apis
+    //get all data get one data get some data [0, 1, many ]
+    app.get('/job-application', async(req, res) => {
+      const email = req.query.email
+      const query = {applicant_email: email}
+      const result = await jobApplicationCollection.find(query).toArray()
+      res.send(result)
+    })
+
     app.post('/job-applications', async(req, res) => {
       const application = req.body 
       const result = await jobApplicationCollection.insertOne(application)
